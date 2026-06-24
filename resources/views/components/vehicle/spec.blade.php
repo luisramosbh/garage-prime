f2e5339ec9f2fd54ba07effcbad7b45ca9b7a172
Luis Santos <luisramosbh@gmail.com>
2026-06-24 09:20:56 -0300

    feat: vehicle details page

---
+++ resources/views/components/vehicle/spec.blade.php
@@ -0,0 +1,9 @@
+<div class="rounded-xl border border-prime-carbon bg-prime-graphite p-5">
+    <p class="text-sm text-prime-muted">
+        {{ $label }}
+    </p>
+
+    <p class="mt-1 text-lg font-bold text-prime-white">
+        {{ $value }}
+    </p>
+</div>
